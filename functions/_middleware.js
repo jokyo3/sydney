cfe891850f92f73f8ccf0e18bc3761415f7b3aa7
jokyo3 <150494320+jokyo3@users.noreply.github.com>
2024-05-18 21:58:11 +0800

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -31,45 +31,40 @@ function handleOptions(request) {
 
 async function handleWebSocket(request) {
   // 在这里添加您的 WebSocket 处理逻辑
-  // ...
-}
-
-async function handleRequest(request, env) {
-  // 在这里添加您的 HTTP 请求处理逻辑
-  // ...
-
-  // 示例：将请求转发到另一个服务器
-  const serverUrl = 'https://sydney.bing.com';
+  let serverUrl = "https://sydney.bing.com";
   const currentUrl = new URL(request.url);
   const fetchUrl = new URL(serverUrl + currentUrl.pathname + currentUrl.search);
-
   let serverRequest = new Request(fetchUrl, request);
   serverRequest.headers.set('origin', 'https://www.bing.com');
   serverRequest.headers.set('referer', 'https://www.bing.com/search?q=Bing+AI');
-
-  // 添加或修改其他头部
   const cookie = serverRequest.headers.get('Cookie') || '';
   let cookies = cookie; 
   if (!cookie.includes('_U=')) {
-      cookies += '; _U=1NssJY8JoQgpNNg8';
+      cookies += '; _U=' + 'xxxxxx';
   }
-
   serverRequest.headers.set('Cookie', cookies);
   serverRequest.headers.set('user-agent', 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.7 Mobile/15E148 Safari/605.1.15 BingSapphire/1.0.410427012');
+  const res = await fetch(serverRequest);
+  const newRes = new Response(res.body, res);
+  newRes.headers.set('Access-Control-Allow-Methods', 'GET,HEAD,POST,OPTIONS');
+  newRes.headers.set('Access-Control-Allow-Credentials', 'true');
+  newRes.headers.set('Access-Control-Allow-Headers', '*');
 
-  const response = await fetch(serverRequest);
-  const newResponse = new Response(response.body, response);
+  // 调试输出连接 IP
+  let Ipurl = new URL("http://ipecho.net/plain");
+  let Ipresponse = await fetch(Ipurl);
+  let textip = await Ipresponse.text();
+  newRes.headers.set('TestLog', "This is Sydney@" + textip);
+  const Guestip = request.headers.get('cf-connecting-ip');
+  newRes.headers.set('Guestip', Guestip); 
 
-  // 设置 CORS 头部
-  newResponse.headers.set('Access-Control-Allow-Origin', '*');
-  newResponse.headers.set('Access-Control-Allow-Methods', 'GET,HEAD,POST,OPTIONS');
-  newResponse.headers.set('Access-Control-Allow-Credentials', 'true');
-  newResponse.headers.set('Access-Control-Allow-Headers', '*');
+  return newRes;
+}
 
-  // 获取客户端 IP 地址
-  const guestIp = request.headers.get('cf-connecting-ip');
-  newResponse.headers.set('Guestip', guestIp);
 
-  // 返回新的响应对象
-  return newResponse;
+async function handleRequest(request, env) {
+  // 在这里添加您的 HTTP 请求处理逻辑
+  const uri = new URL(request.url)
+   uri.hostname = 'sydney.bing.com'
+  return fetch(new Request(uri.toString(), request))
 }
